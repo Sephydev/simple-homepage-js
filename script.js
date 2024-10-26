65a7a8d8419e64ca485c070040e896e48590676e
Sephydev <nicolas.bussiere@outlook.fr>
2024-10-26 16:00:11 +0200

    Add dark theme to theme btn in nav

--- script.js
+++ script.js
@@ -19,12 +19,22 @@ closeNav.addEventListener("click", function(e) {
 themeBtn.addEventListener("click", function(e) {
     const body = document.getElementById("body-theme");
     const logo = document.getElementById("logo");
+    const darkBtn = document.getElementById("dark-btn");
+    const lightBtn = document.getElementById("light-btn");
 
     if (isDark) {
         body.classList.remove("dark");
+        darkBtn.classList.remove("selected");
+        darkBtn.src = "./assets/Moon_fill_light.svg";
+        lightBtn.classList.add("selected");
+        lightBtn.src = "./assets/Sun_fill.svg";
         logo.src = "./assets/alarado-icon-homepage.svg";
     } else {
         body.classList.add("dark");
+        darkBtn.classList.add("selected");
+        darkBtn.src = "./assets/Moon_fill.svg";
+        lightBtn.classList.remove("selected");
+        lightBtn.src = "./assets/Sun_fill_light.svg";
         logo.src = "./assets/alarado-icon-homepage-dark.svg";
     }
 
