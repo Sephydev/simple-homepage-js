aa553159d81189e4de99bce4fad7de7547a1d466
Sephydev <nicolas.bussiere@outlook.fr>
2024-10-26 04:13:47 +0200

    Add logic to close navbar

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const openNav = document.getElementById("open-nav");
+const closeNav = document.getElementById("close-nav");
 
 openNav.addEventListener("click", function(e) {
     const navbar = document.getElementById("navbar");
 
     navbar.classList.remove("hide");
+})
+
+closeNav.addEventListener("click", function(e) {
+    const navbar = document.getElementById("navbar");
+
+    navbar.classList.add("hide");
 })
