94305c13dae3ecf460e0a6e27897e1236484dcea
Sephydev <nicolas.bussiere@outlook.fr>
2024-10-26 15:40:55 +0200

    Add dark theme to body

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const openNav = document.getElementById("open-nav");
 const closeNav = document.getElementById("close-nav");
+const themeBtn = document.getElementById("theme-btn");
+
+let isDark = false;
 
 openNav.addEventListener("click", function(e) {
     const navbar = document.getElementById("navbar");
@@ -11,4 +14,16 @@ closeNav.addEventListener("click", function(e) {
     const navbar = document.getElementById("navbar");
 
     navbar.classList.add("hide");
+})
+
+themeBtn.addEventListener("click", function(e) {
+    const body = document.getElementById("body-theme");
+
+    if (isDark) {
+        body.classList.remove("dark");
+    } else {
+        body.classList.add("dark");
+    }
+
+    isDark = !isDark
 })
